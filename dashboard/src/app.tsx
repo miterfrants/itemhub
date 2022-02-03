11d727970ae7fef1fa26a62c4b04cc8579889414
Liang C <sl20130313@gmail.com>
2022-02-03 22:52:24 +0800

    refactor: delete unnecessary structure

--- dashboard/src/app.tsx
+++ dashboard/src/app.tsx
@@ -1,11 +1,8 @@
 import './app.css';
 import { Outlet } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectDevices } from './redux/reducers/device.reducer';
-import { useContext, useEffect } from 'react';
+import { useEffect } from 'react';
 import { AuthDataservice } from './dataservices/auth.dataservice';
 import { CookieHelper } from './helpers/cookie.helper';
-import { LogContext } from './contexts/logs.context';
 
 function App() {
     // 暫時直接打測試帳號的 API 取得 token，之後可再拔掉
@@ -23,25 +20,7 @@ function App() {
             })();
         }
     }, []);
-    const devices = useSelector(selectDevices);
-
-    const { logs, addLog } = useContext(LogContext);
-    return (
-        <div>
-            <button onClick={() => addLog('testing')}>Add Logs</button>
-            <div>
-                {logs.map((item) => (
-                    <div key={item}>{item}</div>
-                ))}
-            </div>
-            <div>
-                {devices.map((item) => (
-                    <div key={item.name}>{item.name}</div>
-                ))}
-            </div>
-            <Outlet />
-        </div>
-    );
+    return <Outlet />;
 }
 
 export default App;
