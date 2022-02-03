70fecc609973c9ec7e5d944ff6f1a506948903eb
Liang C <sl20130313@gmail.com>
2022-02-03 22:50:30 +0800

    feat: implement SignWithEmail api

--- dashboard/src/app.tsx
+++ dashboard/src/app.tsx
@@ -2,50 +2,39 @@ import './app.css';
 import { Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectDevices } from './redux/reducers/device.reducer';
-// import { add, addMany } from './redux/reducers/device.reducer';
-import { useContext } from 'react';
-// import { AuthDataservice } from './dataservices/auth.dataservice';
-// import { CookieHelper } from './helpers/cookie.helper';
+import { useContext, useEffect } from 'react';
+import { AuthDataservice } from './dataservices/auth.dataservice';
+import { CookieHelper } from './helpers/cookie.helper';
 import { LogContext } from './contexts/logs.context';
 
 function App() {
+    // 暫時直接打測試帳號的 API 取得 token，之後可再拔掉
+    useEffect(() => {
+        const token = CookieHelper.GetCookie('token') || null;
+        if (token === null) {
+            (async () => {
+                AuthDataservice.SignWithEmail({
+                    email: 'miterfrants@gmail.com',
+                    password: '@Testing123123',
+                }).then((response) => {
+                    const tokenValue = response.data.token;
+                    CookieHelper.SetCookie('token', tokenValue, 14);
+                });
+            })();
+        }
+    }, []);
     const devices = useSelector(selectDevices);
+
     const { logs, addLog } = useContext(LogContext);
     return (
         <div>
             <button onClick={() => addLog('testing')}>Add Logs</button>
-
             <div>
                 {logs.map((item) => (
                     <div key={item}>{item}</div>
                 ))}
             </div>
-
-            {/* <div>
-                <button
-                    aria-label="Increment value"
-                    onClick={() => dispatch(increment())}
-                >
-                    +
-                </button>
-                <span>{count}</span>
-                <button
-                    aria-label="Decrement value"
-                    onClick={() => dispatch(decrement())}
-                >
-                    -
-                </button>
-            </div> */}
-
             <div>
-                {/* <button onClick={() => dispatch(add())}>add</button>
-                <button
-                    onClick={() =>
-                        dispatch(addMany([{ name: '2' }, { name: '3' }]))
-                    }
-                >
-                    addMany
-                </button> */}
                 {devices.map((item) => (
                     <div key={item.name}>{item.name}</div>
                 ))}
--- dashboard/src/dataservices/auth.dataservice.ts
+++ dashboard/src/dataservices/auth.dataservice.ts
@@ -2,6 +2,24 @@ import { END_POINT } from '../constants';
 import { ApiHelper } from '../helpers/api.helper';
 
 export const AuthDataservice = {
+    SignWithEmail: async ({
+        email,
+        password,
+    }: {
+        email: string;
+        password: string;
+    }) => {
+        return ApiHelper.SendRequest(
+            `${import.meta.env.VITE_API_ENDPOINT}${END_POINT.SIGN_WITH_EMAIL}`,
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    email,
+                    password,
+                }),
+            }
+        );
+    },
     IsSigned: async (token: string) => {
         return ApiHelper.SendRequestWithToken(
             `${import.meta.env.VITE_API_ENDPOINT}${END_POINT.IS_SIGNED}`,
