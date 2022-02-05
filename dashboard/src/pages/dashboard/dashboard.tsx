6b10020d16b8409b0c032017d8f044ab5f3f9a3b
Liang C <sl20130313@gmail.com>
2022-02-05 20:27:13 +0800

    feat: server 端會判斷 token 是否合法，前端只需要先確認 token 是否存在即可(實作在 API helper 中了)，拔掉 IsSigned API

--- dashboard/src/pages/dashboard/dashboard.tsx
+++ dashboard/src/pages/dashboard/dashboard.tsx
@@ -2,27 +2,20 @@ import { Link, Outlet } from 'react-router-dom';
 import styles from './dashboard.module.scss';
 import Header from '@/components/header/header';
 import Footer from '@/components/footer/footer';
-import { useIsSigned } from '@/hooks/is-signed.hook';
 
 const Dashboard = () => {
-    const { loading } = useIsSigned();
-
     return (
         <div className={styles.dashboard} data-testid="Dashboard">
-            {loading ? (
-                <div>Loading</div>
-            ) : (
-                <div>
-                    <Header />
-                    <nav>
-                        <Link to="/dashboard/devices">Devices</Link> |{' '}
-                        <Link to="/dashboard/triggers">Triggers</Link> |{' '}
-                        <Link to="/dashboard/oauth-clients">oAuth Clients</Link>
-                    </nav>
-                    <Outlet />
-                    <Footer />
-                </div>
-            )}
+            <div>
+                <Header />
+                <nav>
+                    <Link to="/dashboard/devices">Devices</Link> |{' '}
+                    <Link to="/dashboard/triggers">Triggers</Link> |{' '}
+                    <Link to="/dashboard/oauth-clients">oAuth Clients</Link>
+                </nav>
+                <Outlet />
+                <Footer />
+            </div>
         </div>
     );
 };
