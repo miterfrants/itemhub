39c7e33d8c05773a80bb587aeb365ccdb301d600
Liang C <sl20130313@gmail.com>
2022-02-05 16:01:22 +0800

    doc: add reference

--- dashboard/src/hooks/redux.hook.ts
+++ dashboard/src/hooks/redux.hook.ts
@@ -2,5 +2,6 @@ import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import type { RootState, AppDispatch } from '../redux/store';
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
+// reference: https://redux-toolkit.js.org/tutorials/typescript#define-typed-hooks
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
