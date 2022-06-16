feb6ac742fe7523d5f1b45763f52fa6fed282c3c
Peter Huang <miterfrnats@gmail.com>
2022-06-16 20:02:12 +0800

    feat: implement dashboard monitor api

---
+++ api/Constants/DashboardMonitor.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel;
+using Homo.AuthApi;
+
+namespace Homo.IotApi
+{
+    public enum DASHBOARD_MONITOR_MODE
+    {
+        CURRENT_VALUE,
+        LINE_CHART,
+        SWITCH
+    }
+}
