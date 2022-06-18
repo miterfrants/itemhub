56e1f73f9448d6d2f76fd29d84cae80b847c12d6
Peter Huang <miterfrnats@gmail.com>
2022-06-18 16:16:29 +0800

    feat: auto-generated trigger type migration

---
+++ api/Migrations/20220618081022_AddTriggerType.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IotApi.Migrations
+{
+    public partial class AddTriggerType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Trigger",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trigger_Type",
+                table: "Trigger",
+                column: "Type");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Trigger_Type",
+                table: "Trigger");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Trigger");
+
+        }
+    }
+}
