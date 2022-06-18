3a76c08ff64cc936b90cabfbbf270e3dd89f24d9
Peter Huang <miterfrnats@gmail.com>
2022-06-18 22:33:13 +0800

    feat: auto-generated orm migration code

---
+++ api/Migrations/20220618143202_TriggerChangeDestinationDeviceTargetNullableForNotification.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IotApi.Migrations
+{
+    public partial class TriggerChangeDestinationDeviceTargetNullableForNotification : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
