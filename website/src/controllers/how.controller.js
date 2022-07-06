cc0b1f46d1cf0f5331558edc3d970d33896ada48
Peter Huang <miterfrnats@gmail.com>
2022-07-07 00:20:37 +0800

    feat: 優化 rwd how page

--- website/src/controllers/how.controller.js
+++ website/src/controllers/how.controller.js
@@ -143,7 +143,7 @@ export class HowController extends RoutingController {
         });
         if (htmlIndex.length === 0) {
             this.elHTML.querySelector('.index').innerHTML = '';
-            this.pageVariable.howImageVisible = '';
+            this.pageVariable.howImageVisible = 'd-none d-lg-block';
         } else {
             this.elHTML.querySelector('.index').innerHTML = htmlIndex.join('\n');
             this.pageVariable.howImageVisible = 'd-none';
