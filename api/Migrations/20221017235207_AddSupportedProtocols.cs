a17dca8932ae74408514f668cbafbbd38f20f1ad
Peter Huang <miterfrnats@gmail.com>
2022-10-18 07:57:26 +0800

    feat: update dotnet ef for mcu supported protocols

---
+++ api/Migrations/20221017235207_AddSupportedProtocols.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IotApi.Migrations
+{
+    public partial class AddSupportedProtocols : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "SupportedProtocols",
+                table: "Microcontroller",
+                type: "varchar(1024)",
+                maxLength: 1024,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.Sql("UPDATE Microcontroller SET Microcontroller.SupportedProtocols = '[\"http\"]' WHERE `Key` != 'ESP_01S'");
+            migrationBuilder.Sql("UPDATE Microcontroller SET Microcontroller.SupportedProtocols = '[\"http\",\"mqtt\"]' WHERE `Key` = 'ESP_01S'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SupportedProtocols",
+                table: "Microcontroller");
+        }
+    }
+}
