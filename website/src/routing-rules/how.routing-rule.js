9f9e63d77b0d39c330c1df3712bb4b34574da420
Peter Huang <miterfrnats@gmail.com>
2023-04-29 12:46:00 +0800

    feat: 實作把傳統的冷氣變聰明

--- website/src/routing-rules/how.routing-rule.js
+++ website/src/routing-rules/how.routing-rule.js
@@ -4,6 +4,7 @@ import { HowToImplementAutopotController } from '../controllers/how-to-implement
 import { HowToImplementPipelineController } from '../controllers/how-to-implement-pipeline.controller.js';
 import { HowToStartController } from '../controllers/how-to-start.controller.js';
 import { HowController } from '../controllers/how.controller.js';
+import { HowToImplementSmartAcController } from '../controllers/how-to-implement-smart-ac.controller.js';
 
 export const HowRoutingRule = {
     path: 'how/',
@@ -24,5 +25,8 @@ export const HowRoutingRule = {
     }, {
         path: 'implement-pipeline/',
         controller: HowToImplementPipelineController
+    }, {
+        path: 'implement-smart-ac/',
+        controller: HowToImplementSmartAcController
     }]
 };
