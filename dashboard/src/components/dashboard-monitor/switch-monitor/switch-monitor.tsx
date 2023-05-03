232da68ad3a85da2b834f7066e447431911957bf
Peter Huang <miterfrnats@gmail.com>
2023-05-04 07:42:51 +0800

    refactor: update rwd of switch button

--- dashboard/src/components/dashboard-monitor/switch-monitor/switch-monitor.tsx
+++ dashboard/src/components/dashboard-monitor/switch-monitor/switch-monitor.tsx
@@ -3,9 +3,12 @@ import {
     useUpdateDeviceSwitchPinApi,
     useGetDevicePinApi,
 } from '@/hooks/apis/device-pin.hook';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { PinItem } from '@/types/devices.type';
 import Spinner from '@/components/spinner/spinner';
+import debounce from 'lodash.debounce';
+import { useAppSelector } from '@/hooks/redux.hook';
+import { selectLayout } from '@/redux/reducers/layout.reducer';
 
 const SwitchMonitor = (props: { deviceId: number; pin: string }) => {
     const { deviceId, pin } = props;
@@ -29,10 +32,53 @@ const SwitchMonitor = (props: { deviceId: number; pin: string }) => {
         pin,
     });
 
+    const elementContainerRef = useRef<HTMLDivElement>(null);
+    const toggleButtonRef = useRef<HTMLDivElement>(null);
+    const [scale, setScale] = useState<number>(1);
+    const [titleVerticalOffset, setTitleVerticalOffset] = useState<number>(0);
+    const layout = useAppSelector(selectLayout);
+
+    const resizeHandler = useRef(
+        debounce(() => {
+            const containerWidth =
+                elementContainerRef.current?.offsetWidth || 0;
+            const boxShadowWidth = 4;
+            const toggleButtonWidth = toggleButtonRef.current?.offsetWidth || 0;
+            const horizontalPadding = 80;
+            if (!toggleButtonRef.current) {
+                return;
+            }
+            let targetScale =
+                (containerWidth - horizontalPadding) /
+                (toggleButtonWidth + boxShadowWidth);
+            const maxScale = 10;
+            const minScale = 1.3;
+
+            if (targetScale > maxScale) {
+                targetScale = maxScale;
+            } else if (targetScale < minScale) {
+                targetScale = minScale;
+            }
+
+            setTitleVerticalOffset(targetScale * 10);
+            setScale(targetScale);
+        }, 800)
+    );
+
     useEffect(() => {
         getDevicePin();
+        resizeHandler.current();
+        window.addEventListener('resize', resizeHandler.current);
+        return () => {
+            window.removeEventListener('resize', resizeHandler.current);
+        };
+
         // eslint-disable-next-line
     }, []);
+
+    useEffect(() => {
+        resizeHandler.current();
+    }, [layout]);
 
     useEffect(() => {
         if (isFirstTimeGetPinValue) {
@@ -53,7 +99,8 @@ const SwitchMonitor = (props: { deviceId: number; pin: string }) => {
 
     return (
         <div
-            className="switch-monitor w-100 p-3"
+            ref={elementContainerRef}
+            className="switch-monitor w-100 p-3 h-100"
             onClick={() => {
                 setValue(value === 1 ? 0 : 1);
             }}
@@ -63,11 +110,21 @@ const SwitchMonitor = (props: { deviceId: number; pin: string }) => {
                     <Spinner />
                 </div>
             ) : (
-                <div>
-                    <div className="toggle-button-container mt-45">
-                        <Toggle value={value} />
+                <div className="d-flex flex-column h-100 justify-content-center">
+                    <div
+                        className="toggle-button-container d-flex justify-content-center"
+                        style={{ transform: `scale(${scale})` }}
+                    >
+                        <div ref={toggleButtonRef}>
+                            <Toggle value={value} />
+                        </div>
                     </div>
-                    <div className="d-flex justify-content-center device-name text-center">
+                    <div
+                        className="d-flex title justify-content-center device-name text-center"
+                        style={{
+                            transform: `translateY(${titleVerticalOffset}px)`,
+                        }}
+                    >
                         <span className="d-none d-md-inline ps-2">
                             <div
                                 className={`align-middle dot rounded-circle ${
