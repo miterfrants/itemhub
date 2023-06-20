69b992be401c7ccedff598b3c0f0ad495128812f
Peter Huang <miterfrnats@gmail.com>
2023-06-20 19:41:59 +0800

    feat: 修正 in app browser 無法正常登入

--- website/src/controllers/sign-in.controller.js
+++ website/src/controllers/sign-in.controller.js
@@ -17,7 +17,12 @@ export class SignInController extends RoutingController {
         this.meta = {
             title: '登入 - ItemHub'
         };
+        const standalone = window.navigator.standalone;
+        const userAgent = window.navigator.userAgent.toLowerCase();
+        const safari = /safari/.test(userAgent);
+        const ios = /iphone|ipod|ipad/.test(userAgent);
         await super.render({
+            isInApp: ios && !standalone && !safari
         });
     }
 
@@ -99,7 +104,11 @@ export class SignInController extends RoutingController {
     }
 
     async signInWithLine () {
-        PopupHelper.PopupCenter(`https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${THIRD_PARTY_KEY.LINE_CLIENT_ID}&redirect_uri=${window.location.origin}/oauth/&state=${OAUTH_PROVIDER.LINE},${OAUTH_TYPE.SIGN_IN}&bot_prompt=aggressive&scope=profile%20openid%20email`, 'line auth', 600, 500);
+        if (!this.pageVariable.isInApp) {
+            PopupHelper.PopupCenter(`https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${THIRD_PARTY_KEY.LINE_CLIENT_ID}&redirect_uri=${window.location.origin}/oauth/&state=${OAUTH_PROVIDER.LINE},${OAUTH_TYPE.SIGN_IN}&bot_prompt=aggressive&scope=profile%20openid%20email`, 'line auth', 600, 500);
+        } else {
+            location.href = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${THIRD_PARTY_KEY.LINE_CLIENT_ID}&redirect_uri=${window.location.origin}/oauth/&state=${OAUTH_PROVIDER.LINE},${OAUTH_TYPE.SIGN_IN}&bot_prompt=aggressive&scope=profile%20openid%20email`;
+        }
     }
 
     async signInWithGoogle () {
@@ -108,6 +117,10 @@ export class SignInController extends RoutingController {
             provider: OAUTH_PROVIDER.GOOGLE
         };
         const url = ['https://accounts.google.com/o/oauth2/v2/auth?1=1', 'scope=https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile', 'include_granted_scopes=true', 'response_type=code', `state=${JSON.stringify(state)}`, `redirect_uri=${location.origin}/oauth/`, `client_id=${THIRD_PARTY_KEY.GOOGLE_CLIENT_ID}`].join('&');
-        PopupHelper.PopupCenter(url, 'google auth', 600, 500);
+        if (!this.pageVariable.isInApp) {
+            PopupHelper.PopupCenter(url, 'google auth', 600, 500);
+        } else {
+            location.href = url;
+        }
     }
 }
