c5acaaa2b2e335bcd03c474033cb44e461569072
Peter Huang <miterfrnats@gmail.com>
2023-08-12 01:19:37 +0800

    feat: 實作新增修改 ComputedFunction

--- api/Controllers/MyComputedFunctionController.cs
+++ api/Controllers/MyComputedFunctionController.cs
@@ -24,5 +24,29 @@ namespace Homo.IotApi
         {
             return ComputedFunctionDataservice.GetAll(_dbContext, extraPayload.Id, devicePins, monitorIds);
         }
+
+        [SwaggerOperation(
+            Tags = new[] { "感測資料即時轉換" },
+            Summary = "感測資料即時轉換 - 新增",
+            Description = ""
+        )]
+        [HttpPost]
+        public ActionResult<dynamic> add([FromBody] DTOs.CreateComputedFunction dto, Homo.AuthApi.DTOs.JwtExtraPayload extraPayload)
+        {
+            return ComputedFunctionDataservice.Create(_dbContext, extraPayload.Id, dto);
+        }
+
+        [SwaggerOperation(
+            Tags = new[] { "感測資料即時轉換" },
+            Summary = "感測資料即時轉換 - 更新",
+            Description = ""
+        )]
+        [HttpPatch]
+        [Route("{id}")]
+        public ActionResult<dynamic> update([FromRoute] long id, [FromBody] DTOs.UpdateComputedFunction dto, Homo.AuthApi.DTOs.JwtExtraPayload extraPayload)
+        {
+            ComputedFunctionDataservice.Update(_dbContext, extraPayload.Id, id, dto);
+            return new { status = "OK" };
+        }
     }
 }
