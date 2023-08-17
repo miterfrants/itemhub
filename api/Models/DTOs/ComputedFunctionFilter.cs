adfe998a9b890d0005050f5401a67dd201d008fb
Peter Huang <miterfrnats@gmail.com>
2023-08-17 11:32:02 +0800

    feat: 加上 source sensor device id 和 source sensor pin 作為前端去拿資料的依據

--- api/Models/DTOs/ComputedFunctionFilter.cs
+++ api/Models/DTOs/ComputedFunctionFilter.cs
@@ -16,13 +16,17 @@ namespace Homo.IotApi
             public long? groupId { get; set; }
             public string func { get; set; }
             public COMPUTED_TARGET target { get; set; }
+            public COMPUTED_SOURCE source { get; set; }
+            public long? sourceDeviceId { get; set; }
+            public string sourcePin { get; set; }
         }
 
 
         public partial class UpdateComputedFunction : DTOs
         {
             public string func { get; set; }
+            public long? sourceDeviceId { get; set; }
+            public string sourcePin { get; set; }
         }
-
     }
 }
